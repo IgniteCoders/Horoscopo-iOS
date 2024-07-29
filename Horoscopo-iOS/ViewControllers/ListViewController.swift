93d6a9c525ef77024e2d34e8d15821c7904fd45f
Mansour <theboom.kajas@gmail.com>
2024-07-29 12:55:32 +0200

    Now we list the horoscopes in a TableView with custom cell

--- Horoscopo-iOS/ViewControllers/ListViewController.swift
+++ Horoscopo-iOS/ViewControllers/ListViewController.swift
@@ -7,13 +7,34 @@
 
 import UIKit
 
-class ListViewController: UIViewController {
-
+class ListViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var horoscopeList: [Horoscope] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        horoscopeList = HoroscopeProvider.getAllHoroscopes()
+        
+        tableView.dataSource = self
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return horoscopeList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! HoroscopeViewCell
+        
+        let horoscope = horoscopeList[indexPath.row]
+        
+        cell.nameLabel.text = horoscope.name
+        cell.logoImageView.image = horoscope.logo
+        cell.datesLabel.text = horoscope.dates
+        
+        return cell
+    }
 }
 
